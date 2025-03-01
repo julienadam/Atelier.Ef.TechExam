ab4b492ade3a67e955942fa619ae73f233b6672c
Julien Adam <julien.adam@arcodia.fr>
2025-03-01 10:39:58 +0100

    Lab 1 : Migrations

---
+++ Entities/Sujet.cs
@@ -0,0 +1,8 @@
+﻿namespace Atelier.Ef.TechExam.Entities
+{
+    public class Sujet
+    {
+        public int SujetId { get; set; }
+        public required string Titre { get; set; }
+    }
+}
---
+++ Migrations/20250301093318_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Atelier.Ef.TechExam.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sujets",
+                columns: table => new
+                {
+                    SujetId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sujets", x => x.SujetId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sujets");
+        }
+    }
+}
